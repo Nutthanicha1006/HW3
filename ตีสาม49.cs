66fde55a2407d1d13c65bd6da2ab0038548830ba
LAPTOP-2QNNL6VT\Lenovo <nutthanicha.kmutt@mail.kmutt.ac.th>
2021-03-12 03:59:26 +0700

    no message

---
+++ ตีสาม49.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode;
+            double X, Y, t1, p1, t2, p2;
+
+            Console.Write("");
+            mode = Console.ReadLine();
+            Console.Write("");
+            X = double.Parse(Console.ReadLine());
+            Console.Write("");
+            Y = double.Parse(Console.ReadLine());
+
+            if (mode == "time")
+            {
+                if (X >= 0)
+                {
+                    t1 = X;
+                    p1 = Math.Pow((X - 1), 2);
+                    Console.WriteLine("{0}, {1}", t1, p1);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+            }
+            else if (mode == "price")
+            {
+                if (X >= 0)
+                {
+                    t2 = (Math.Sqrt(Y) + 1);
+                    p2 = Y;
+                    Console.WriteLine("{0}, {1}", t2, p2);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid mode");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
